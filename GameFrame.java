d4cc2bf580bd2cb5fd6279e15cfa7704674622a9
FatmaD21 <88843080+FatmaD21@users.noreply.github.com>
2022-05-29 20:23:08 +0530

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class GameFrame extends JFrame {
+
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("SNAKE GAME");//Title of game
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
+
+
+
+
